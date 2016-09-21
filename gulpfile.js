10630767de3d8e75a3544eeb77ec3265f39ac424
Vinicius Salvati Melquiades <vinicius.melquiades@outlook.com>
2016-09-21 14:53:24 -0300

    Project configurations
    - Added configuration for VS Code (only whitespace, for now).
    - NPM Scripts for publishing and updating the version

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
-const gulp = require('gulp'),
+const
+	gulp = require('gulp'),
+	util = require('gulp-util'),
 	concat = require('gulp-concat'),
 	ts = require('gulp-typescript'),
 	sass = require('gulp-sass'),
 	merge = require('merge2'),
-	replace = require('gulp-replace');
+	replace = require('gulp-replace'),
+	gulpIf = require('gulp-if');
+
+const release = !!(util.env.r || util.env.release);
+
+if (release) {
+	util.log(`Running in ${util.colors.blue('RELEASE')} mode`);
+}
 
 const compiledModuleSeparator = `
 })(ngSchedule || (ngSchedule = {}));
 var ngSchedule;
 (function (ngSchedule) {
 `;
-var tsProject = ts.createProject('tsconfig.json');
+const tsProject = ts.createProject('tsconfig.json');
 gulp.task('ts', function () {
 	const projectSrc = gulp.src([
 		'src/scripts/*.ts'])
@@ -19,13 +28,15 @@ gulp.task('ts', function () {
 	return merge([projectSrc, gulp.src("typings/**/*.ts")])
 		.pipe(ts(tsProject))
 		.pipe(replace(compiledModuleSeparator, '\n\n'))
-		.pipe(gulp.dest('build'));
+		.pipe(gulp.dest('build'))
+		.pipe(gulpIf(release, gulp.dest('./')));
 });
 
 gulp.task('sass', function () {
 	return gulp.src('src/styles/*.scss')
 		.pipe(sass())
-		.pipe(gulp.dest('build'));
+		.pipe(gulp.dest('build'))
+		.pipe(gulpIf(release, gulp.dest('./')));
 });
 
 gulp.task('default', ['ts', 'sass']);
